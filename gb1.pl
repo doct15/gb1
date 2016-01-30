15d41afc8140e765aaa65ff6873839ec24243ad8
Brian McGehee <doc@tavian.com>
2016-01-29 21:59:49 -0800

    fixes

--- gb1.pl
+++ gb1.pl
@@ -8,7 +8,6 @@ use Data::Dumper;
 print "Content-type: text/html\n\n";
 
 $in_url=CGI->new->url();
-#$in_server = param("server_name");
 $in_server = $ENV{SERVER_NAME};
 $repo_owner = param("repo_owner");
 $repo_name = param("repo_name");
@@ -31,25 +30,14 @@ $response = $temp[1];
 
 print Dumper(@test);
 
-$redirect_url="http://doc.tavian.com/build-unknown-yellow.svg";
-if ($response eq "pending") { $redirect_url = "http://doc.tavian.com/build-pending-yellow.svg"; }
-if ($response eq "failure") { $redirect_url = "http://doc.tavian.com/build-failure-red.svg"; }
-if ($response eq "error") { $redirect_url = "http://doc.tavian.com/build-error-red.svg"; }
-if ($response eq "success") { $redirect_url = "http://doc.tavian.com/build-success-green.svg"; }
+$redirect_url="http://$in_server/build-unknown-yellow.svg";
+
+if ($response eq "pending") { $redirect_url = "http://$in_server/build-pending-yellow.svg"; }
+if ($response eq "failure") { $redirect_url = "http://$in_server/build-failure-red.svg"; }
+if ($response eq "error") { $redirect_url = "http://$in_server/build-error-red.svg"; }
+if ($response eq "success") { $redirect_url = "http://$in_server/build-success-green.svg"; }
 
 print "$redirect_url<br>";
 
 print "Location: $redirect_url\n\n";
-
-sub Error_handler {
-	$errormsg = $_[0];
-	$im = new GD::Image(500,32) || die;
-	$black = $im->colorAllocate(0,0,0);
-	$white = $im->colorAllocate(255,255,255);
-	$im->transparent($black);
-	$im->fill(10,10,$black);
-	$im->string(gdSmallFont,0,0,$errormsg,$white);
-	print $im->gif;
-	die;
-}
 
--- gb2.pl
+++ gb2.pl
@@ -1,28 +1,43 @@
 #!/usr/bin/perl
 #
-#
 
 use CGI qw(param);
 use CGI;
 use Data::Dumper;
 
+
+
 $in_url=CGI->new->url();
-$in_server=CGI->new->server_name();
+$in_server = $ENV{SERVER_NAME};
 $repo_owner = param("repo_owner");
 $repo_name = param("repo_name");
 $repo_branch = param("repo_branch");
 
+
+
+
+
+
+
+
+
 $response = `curl -q -k "https://api.github.com/repos/$repo_owner/$repo_name/status/$repo_branch" | jq '.state'`;
+
+
 
 @temp = split /"/, $response;
 $response = $temp[1];
 
-$redirect_url="http://doc.tavian.com/build-unknown-yellow.svg";
 
-if ($response eq "pending") { $redirect_url = "http://doc.tavian.com/build-pending-yellow.svg"; }
-if ($response eq "failure") { $redirect_url = "http://doc.tavian.com/build-failure-red.svg"; }
-if ($response eq "error") { $redirect_url = "http://doc.tavian.com/build-error-red.svg"; }
-if ($response eq "success") { $redirect_url = "http://doc.tavian.com/build-success-green.svg"; }
+
+$redirect_url="http://$in_server/build-unknown-yellow.svg";
+
+if ($response eq "pending") { $redirect_url = "http://$in_server/build-pending-yellow.svg"; }
+if ($response eq "failure") { $redirect_url = "http://$in_server/build-failure-red.svg"; }
+if ($response eq "error") { $redirect_url = "http://$in_server/build-error-red.svg"; }
+if ($response eq "success") { $redirect_url = "http://$in_server/build-success-green.svg"; }
+
+
 
 print "Location: $redirect_url\n\n";
 
